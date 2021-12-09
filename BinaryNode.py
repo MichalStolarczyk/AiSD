473ef8c228b6669b7434a7b6467f83caf0f6e486
MichalStolarczyk <95419151+MichalStolarczyk@users.noreply.github.com>
2021-12-09 17:57:39 +0100

    update

--- BinaryNode.py
+++ BinaryNode.py
@@ -23,24 +23,3 @@ class BinaryNode:
 
     def add_right_child(self, value: Any):
         self.right_child = BinaryNode(value)
-
-    def traverse_in_order(self, visit: Callable[[Any], None]):
-        if self.left_child is not None:
-            self.left_child.traverse_in_order(visit)
-        visit(self)
-        if self.right_child is not None:
-            self.right_child.traverse_in_order(visit)
-
-    def traverse_post_order(self, visit: Callable[[Any], None]):
-        if self.left_child is not None:
-            self.left_child.traverse_post_order(visit)
-        if self.right_child is not None:
-            self.right_child.traverse_post_order(visit)
-        visit(self)
-
-    def traverse_pre_order(self, visit: Callable[[Any], None]):
-        visit(self)
-        if self.left_child is not None:
-            self.left_child.traverse_pre_order(visit)
-        if self.right_child is not None:
-            self.right_child.traverse_pre_order(visit)
