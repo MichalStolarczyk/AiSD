cdf183e98915e7cca7512c09450e04c0d811cdec
MichalStolarczyk <95419151+MichalStolarczyk@users.noreply.github.com>
2021-12-09 17:58:29 +0100

    update

--- BinaryTree.py
+++ BinaryTree.py
@@ -25,3 +25,16 @@ drzewo.root.left_child.add_left_child(3)
 drzewo.root.left_child.add_right_child(8)
 drzewo.root.left_child.left_child.add_left_child(4)
 print(left_line(drzewo))
+
+tree = BinaryTree(10)
+assert tree.root.value == 10
+tree.root.add_right_child(2)
+assert tree.root.right_child.value == 2
+tree.root.right_child.add_right_child(1)
+assert tree.root.right_child.is_leaf() is False
+tree.root.add_left_child(1)
+tree.root.left_child.add_left_child(1)
+assert tree.root.left_child.left_child.value == 1
+assert tree.root.left_child.left_child.is_leaf() is True
+tree.root.left_child.add_right_child(1)
+tree.root.right_child.add_left_child(1)
